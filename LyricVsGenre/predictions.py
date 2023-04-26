0f6172db1ab575cf6b15ea20a82e413e415dc123
Kacey MacCutcheon <maccut56@students.rowan.edu>
2023-04-25 20:37:45 -0400

    Finding top 3 adjusted

--- LyricVsGenre/predictions.py
+++ LyricVsGenre/predictions.py
@@ -28,36 +28,20 @@ def predict(lyrics):
             for genre in result:
                 total = counts[genre]
                 count = data[word][genre]
-                genre_percent[genre] = genre_percent[genre] * (count/total) * 100
+                genre_percent[genre] = genre_percent[genre] * (count / total) * 100
 
-    high1 = 0
-    high2 = 0
-    high3 = 0
-    highest = []
     total = 0
 
     for genre in genre_percent:
         total += genre_percent[genre]
-        if float(genre_percent[genre]) > high1:
-            high1 = genre_percent[genre]
 
+    genre_lists = []
     for genre in genre_percent:
-        if high2 < float(genre_percent[genre]) < high1:
-            high2 = genre_percent[genre]
+        genre_lists.append([genre, genre_percent[genre]/total * 100])
 
-    for genre in genre_percent:
-        if high3 < float(genre_percent[genre]) < high2 < float(genre_percent[genre]) < high1:
-            high3 = genre_percent[genre]
+    genre_lists.sort(key=lambda x: x[1], reverse=True)
 
-    for genre in genre_percent:
-        if genre_percent[genre] == high1 or genre_percent[genre] == high2 or genre_percent[genre] == high3:
-            highest.append([genre, genre_percent[genre]])
+    return genre_lists[:3]
 
-    tmp = []
-    tmp.append([highest[0][0], str(highest[0][1]/total*100)+'%'])
-    tmp.append([highest[1][0], str(highest[1][1] / total*100)+'%'])
-    tmp.append([highest[2][0], str(highest[2][1] / total*100)+'%'])
-
-    return tmp
 
 print(predict('on a dark desert highway cool wind though my hair'))
